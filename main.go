4c5d2aa391e9e708a256f43b9b2439100791191e
Cat who code <codestudio_one@yahoo.co.id>
2026-02-03 04:35:22 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import ( 
+	"encoding/json"
+	"net/http"
+	"os"
+	"fmt"
+	"log"
+	"strings"
+	"kasir-api/database"
+	"kasir-api/repositories"
+	"kasir-api/services"
+	"kasir-api/handlers"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port    string `mapstructure:"PORT"`
+	DBConn string `mapstructure:"DB_CONN"`
+}
+
+
+
+func main(){
+
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if _, err := os.Stat(".env"); err == nil {
+		viper.SetConfigFile(".env")
+		_ = viper.ReadInConfig()
+	}
+
+	config := Config{
+		Port:   viper.GetString("PORT"),
+		DBConn: viper.GetString("DB_CONN"),
+	}
+
+	db, err := database.InitDB(config.DBConn)
+
+	if err != nil {
+		log.Fatal("Failed to initialize database:", err)
+	}
+	
+	defer db.Close()
+
+	categoryRepo := repositories.NewCategoryRepository(db)
+	categoryService := services.NewCategoryService(categoryRepo)
+	categoryHandler := handlers.NewCategoryHandler(categoryService)
+
+	productRepo := repositories.NewProductRepository(db)
+	productService := services.NewProductService(productRepo)
+	productHandler := handlers.NewProductHandler(productService)
+
+	http.HandleFunc("/api/categories", categoryHandler.HandleCategories)
+	http.HandleFunc("/api/categories/", categoryHandler.HandleCategoryByID)
+
+	http.HandleFunc("/api/produk", productHandler.HandleProducts)
+	http.HandleFunc("/api/produk/", productHandler.HandleProductByID)
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{
+			"status":  "OK",
+			"message": "API Running",
+		})
+	})
+
+	fmt.Println("Server running at " + config.Port)
+
+	err = http.ListenAndServe(":" + config.Port, nil)
+
+	if err != nil {
+		fmt.Println("Server failed to run")
+	}
+}
---
+++ repositories/product_repository.go
@@ -0,0 +1,99 @@
+package repositories
+
+import (
+	"database/sql"
+	"errors"
+	"kasir-api/models"
+)
+
+type ProductRepository struct {
+	db *sql.DB
+}
+
+func NewProductRepository(db *sql.DB) *ProductRepository {
+	return &ProductRepository{db: db}
+}
+
+func (repo *ProductRepository) GetAll() ([]models.Product, error) {
+	query := "SELECT products.id, products.name,  categories.name AS category, products.price, products.stock FROM products INNER JOIN categories ON products.category_id=categories.id"
+	rows, err := repo.db.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	products := make([]models.Product, 0)
+	for rows.Next() {
+		var p models.Product
+		err := rows.Scan(&p.ID, &p.Name, &p.Category, &p.Price, &p.Stock)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+
+	return products, nil
+}
+
+func (repo *ProductRepository) Create(product *models.ProductWrite) error {
+	query := "INSERT INTO products (name, category_id, price, stock) VALUES ($1, $2, $3, $4) RETURNING id"
+	err := repo.db.QueryRow(query, product.Name, product.CategoryID, product.Price, product.Stock).Scan(&product.ID)
+	return err
+}
+
+// GetByID - ambil produk by ID
+func (repo *ProductRepository) GetByID(id int) (*models.Product, error) {
+	query := "SELECT products.id, products.name, categories.name AS category, products.price, products.stock FROM products INNER JOIN categories ON products.category_id=categories.id WHERE products.id = $1"
+
+	var p models.Product
+	err := repo.db.QueryRow(query, id).Scan(&p.ID, &p.Name, &p.Category, &p.Price, &p.Stock)
+	if err == sql.ErrNoRows {
+		return nil, errors.New("produk tidak ditemukan")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &p, nil
+}
+
+func (repo *ProductRepository) Update(product *models.ProductWrite) error {
+	query := "UPDATE products SET name = $1, category_id = $2, price = $3, stock = $4 WHERE id = $5"
+	result, err := repo.db.Exec(query, product.Name, product.CategoryID, product.Price, product.Stock, product.ID)
+	
+	if err != nil {
+		return err
+	}
+
+	rows, err := result.RowsAffected()
+	
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return errors.New("produk tidak ditemukan")
+	}
+
+	return nil
+}
+
+func (repo *ProductRepository) Delete(id int) error {
+	query := "DELETE FROM products WHERE id = $1"
+	result, err := repo.db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return errors.New("produk tidak ditemukan")
+	}
+
+	return err
+}
